eb1517131138e426b4856b3fe6cadbeea8f2b4ce
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-14 00:26:15 -0700

    Create string_utils.h

---
+++ src/utils/string_utils.h
@@ -0,0 +1,12 @@
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+#include <string>
+#include <algorithm>
+
+class StringUtils {
+public:
+    static std::string toLower(const std::string &str);
+};
+
+#endif // STRING_UTILS_H
